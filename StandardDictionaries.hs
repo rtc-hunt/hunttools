b821a4b57490a4e4f007d34248c351ebc008e309
Jonathan D.K. Gibbons <jonored@gmail.com>
2017-01-19 11:37:16 -0500

    Use data paths for standard dictionaries...

--- StandardDictionaries.hs
+++ StandardDictionaries.hs
@@ -1,41 +1,38 @@
 {-# LANGUAGE Trustworthy #-}
-module StandardDictionaries (sysDict, ukacd, enwikt, sowpods, wiki, onelook, onelookR) where
+module StandardDictionaries where
+--module StandardDictionaries (sysDict, ukacd, onelook, sowpods) where
 import Dictionary
 import System.IO.Unsafe
 import Data.Char
 import qualified Crossword
 import Data.DAWG.Packed64
 import Anagram
-
+import Paths_hunttools_dicts
+import qualified Paths_hunttools as H
+-- import qualified Language.Lojban.Jbovlaste as JBO
 
 sysDictGen = Crossword.buildDict $ words $ map toLower $ unsafePerformIO $ readFile "/usr/share/dict/words"
 sysDictDef = Dictionary 
-	(AnagramDictionary $ unsafePerformIO $ fromFile "/home/jonored/huntrepl/dicts/dictWordsAnagram.dawg")
-	(Crossword.BidirectionalDictionary $ unsafePerformIO $ fromFile "/home/jonored/huntrepl/dicts/dictWordsCross.dawg")
+	(AnagramDictionary $ unsafePerformIO $ getDataFileName "dictWordsAnagram.dawg" >>= fromFile)
+	(Crossword.BidirectionalDictionary $ unsafePerformIO $ getDataFileName "dictWordsCross.dawg" >>= fromFile)
 
 sysDict :: (DictionaryContext a) => a
 sysDict = fromDictionary sysDictDef
 
 ukacdDictDef = Dictionary
-  (AnagramDictionary $ unsafePerformIO $ fromFile "/home/jonored/huntrepl/dicts/ukacd-anagram.dawg")
-  (Crossword.BidirectionalDictionary $ unsafePerformIO $ fromFile "/home/jonored/huntrepl/dicts/ukacd-cross.dawg")
+  (AnagramDictionary $ unsafePerformIO $ getDataFileName "ukacd-anagram.dawg" >>= fromFile)
+  (Crossword.BidirectionalDictionary $ unsafePerformIO $ getDataFileName "ukacd-cross.dawg" >>= fromFile)
 
 ukacd :: (DictionaryContext a) => a
 ukacd = fromDictionary ukacdDictDef
 
-enwiktDictDef = Dictionary
+{-enwiktDictDef = Dictionary
   (AnagramDictionary $ unsafePerformIO $ fromFile "/home/jonored/huntrepl/dicts/enwikt-anagram.dawg")
   (Crossword.BidirectionalDictionary $ unsafePerformIO $ fromFile "/home/jonored/huntrepl/dicts/enwikt-cross.dawg")
 
 enwikt :: (DictionaryContext a) => a
 enwikt = fromDictionary enwiktDictDef
 
-sowpodsDictDef = Dictionary
-  (AnagramDictionary $ unsafePerformIO $ fromFile "/home/jonored/huntrepl/dicts/sowpods-anagram.dawg")
-  (Crossword.BidirectionalDictionary $ unsafePerformIO $ fromFile "/home/jonored/huntrepl/dicts/sowpods-cross.dawg")
-
-sowpods :: (DictionaryContext a) => a
-sowpods = fromDictionary sowpodsDictDef
 
 wikiDictDef = Dictionary
   (AnagramDictionary $ unsafePerformIO $ fromFile "/home/jonored/huntrepl/dicts/wiki-anagram.dawg")
@@ -45,13 +42,23 @@ wiki :: (DictionaryContext a) => a
 wiki = fromDictionary wikiDictDef
 
 onelookDictDefR = Dictionary
-  (AnagramDictionary $ unsafePerformIO $ fromFile "/home/jonored/huntrepl/dicts/onelook-justletters.anagram.dawg")
-  (Crossword.BidirectionalDictionary $ unsafePerformIO $ fromFile "/home/jonored/huntrepl/dicts/onelook-justletters.cross.dawg")
+  (AnagramDictionary $ unsafePerformIO $ fromFile $ getDataFileName "/home/jonored/huntrepl/dicts/onelook-justletters.anagram.dawg")
+  (Crossword.BidirectionalDictionary $ unsafePerformIO $ fromFile $ getDataFileName "/home/jonored/huntrepl/dicts/onelook-justletters.cross.dawg")
 onelookR :: (DictionaryContext a) => a
 onelookR = fromDictionary onelookDictDefR
+-}
 
 onelookDictDef = Dictionary
-  (AnagramDictionary $ unsafePerformIO $ mapFile "/home/jonored/huntrepl/dicts/onelook-justletters.anagram.mm.dawg")
-  (Crossword.BidirectionalDictionary $ unsafePerformIO $ mapFile "/home/jonored/huntrepl/dicts/onelook-justletters.cross.mm.dawg")
+  (AnagramDictionary $ unsafePerformIO $ getDataFileName "onelook-justletters.anagram.mm.dawg" >>= mapFile)
+  (Crossword.BidirectionalDictionary $ unsafePerformIO $ getDataFileName "onelook-justletters.cross.mm.dawg" >>= mapFile)
 onelook :: (DictionaryContext a) => a
 onelook = fromDictionary onelookDictDef
+
+{-sowpodsDictDef = Dictionary
+  (AnagramDictionary $ unsafePerformIO $ getDataFileName "sowpods-anagram.dawg" >>= fromFile)
+  (Crossword.BidirectionalDictionary $ unsafePerformIO $ getDataFileName "sowpods-cross.dawg" >>= fromFile)
+
+sowpods :: (DictionaryContext a) => a
+sowpods = fromDictionary sowpodsDictDef
+-}
+-- jbovlaste = unsafePerformIO $ H.getDataFileName "jbovlaste.db" >>= JBO.readDB
