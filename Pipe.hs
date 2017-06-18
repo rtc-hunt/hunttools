49f844f5021fea3ab7f64b20bf555044144d9f88
Jonathan D.K. Gibbons <jonored@gmail.com>
2017-06-18 18:56:13 -0400

    Clearing out the branch, so that we can generate docs into here.

--- Pipe.hs
+++
@@ -1,4 +0,0 @@
-module Pipe where
-
-infixl 0 $|
-a $| b = b a
--- Vowels.hs
+++
@@ -1,4 +0,0 @@
-isVowel = (`elem` "aeiouAEIOU")
-isConsonant = not . isVowel
-intervowel wd = [vls] ++ intersperse vls [Literal [a]|a<-filter isConsonant wd] ++ [vls]
-	where vls = Star (Charset "aeiou")
